9785a8496356088bb7a8a386f33a704c2814a746
RunningCx <2191322090@qq.com>
2023-06-04 10:39:22 +0800

    初始化化工程

---
+++ middleware/server/src/main/java/com/debug/middleware/server/MainApplication.java
@@ -0,0 +1,22 @@
+package com.debug.middleware.server;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.debug.middleware.model")
+public class MainApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
